2dc9536847fec60e718d3fe7d6d2a9f9f6eee2e8
Santanu Sikder <65644599+studsantanu@users.noreply.github.com>
2020-09-26 13:13:08 +0530

    Add the basic code for Stack

---
+++ stack.py
@@ -0,0 +1,42 @@
+# Array-based implementation of stack data structure
+class Stack:
+	# Initialize the stack as an empty array
+	def __init__(self):
+		self.stack = []
+
+	# Check if the stack is empty
+	def isEmpty(self):
+		return len(self.stack) == 0
+
+	# Append a new value at the end of the array, i.e., on the top of the stack
+	def push(self, val):
+		self.stack.append(val)
+
+	# Remove the top element of the stack and returns it, if the stack is not empty
+	def pop(self):
+		if not self.isEmpty():
+			return self.stack.pop()
+
+	# Return the top element without removing it
+	def peek(self):
+		if not self.isEmpty():
+			return self.stack[-1]
+
+# Test it
+if __name__ == "__main__":
+	stack = Stack()
+	stack.push(1)
+	stack.push(54)
+	stack.push(43)
+	stack.push(3)
+	print(str(stack.pop()))
+	stack.push(5)
+	stack.pop()
+	stack.push(2)
+	print(str(stack.peek()))
+	stack.pop()
+	stack.pop()
+	stack.pop()
+	print(str(stack.pop()))
+	print(stack.isEmpty())
+
